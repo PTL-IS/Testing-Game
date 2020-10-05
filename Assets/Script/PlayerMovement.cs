6522f76fa56f4e90044e40093124d860534e7e3d
Hayden Prairie <hprairie17@gmail.com>
2020-10-05 15:23:56 -0500

    Fixed a couple of things

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,7 +9,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("MoveHorizontal"), Input.GetAxis("MoveVertical"), 0.0f);
+        //Vector3 movement = new Vector3(Input.GetAxis("MoveHorizontal"), Input.GetAxis("MoveVertical"), 0.0f); // This is used for Xbox controlls. Right Now you can only really see the walk animation if you use xbox controls.
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f); //This is used for PC controlls. Right Now you can only really see the walk animation if you use xbox controls.
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
