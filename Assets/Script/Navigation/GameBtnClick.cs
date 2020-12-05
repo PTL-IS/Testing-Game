93fd5d041e48f15da9884265dbab4661dc89a704
Hayden Prairie <hprairie17@gmail.com>
2020-12-05 13:12:18 -0600

    Created Scenes

---
+++ Assets/Script/Navigation/GameBtnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBtnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void gameMenuBtn()
+    {
+        SceneManager.LoadScene("Game Menu");
+    }
+
+    public void mainMenuBtn()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void map1MenuBtn()
+    {
+        SceneManager.LoadScene("Map 1");
+    }
+}
