49f039105ecf6e7637f4143897d3f63d032ac16d
Hayden Prairie <hprairie17@gmail.com>
2020-12-05 15:22:11 -0600

    PauseMenu

---
+++ Assets/Script/Navigation/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    [SerializeField] private bool isPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DecativateMenu();
+        }
+    }
+
+    public void mainMenuBtn()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    void ActivateMenu ()
+    {
+        pauseMenuUI.SetActive(true);
+    }
+
+    void DecativateMenu ()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+}
